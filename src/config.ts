6f5862f42bef41dba6bb1958dbfec1056e740af9
srchap78 <srchap78@gmail.com>
2023-02-26 17:57:32 +0100

    1

--- src/config.ts
+++ src/config.ts
@@ -4,9 +4,16 @@ export default {
   type: Phaser.AUTO,
   parent: 'game',
   backgroundColor: '#33A5E7',
+  physics: {
+    default: 'arcade',
+    arcade: {
+        gravity: { y: 300 },
+        debug: false
+    }
+},
   scale: {
-    width: 800,
-    height: 600,
+    width: 1920,
+    height: 1080,
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH
   }
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -6,19 +6,32 @@ export default class Demo extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('logo', 'assets/phaser3-logo.png');
+    this.load.image('background', 'assets/background.jpg');
+    //Used texture packer to create multiple texture atlases/sprite sheets
+    this.load.multiatlas('breakout', 'assets/breakout.json', 'assets');
+
+
   }
 
   create() {
-    const logo = this.add.image(400, 70, 'logo');
+    this.add.image(960, 540, 'background').setScale(1);
+    
+    this.anims.create({ 
+      key: 'blue', 
+      frames: this.anims.generateFrameNames('breakout ', {
+         suffix: '-Breakout-Tiles.png', 
+         start: 1,
+         end: 7, 
+         zeroPad: 3 
+        }), 
+      repeat: -1 });
 
-    this.tweens.add({
-      targets: logo,
-      y: 350,
-      duration: 1500,
-      ease: 'Sine.inOut',
-      yoyo: true,
-      repeat: -1
-    });
+    var blue = this.add.sprite(100, 100, 'breakout').setScale(.25).play('blue')
+    
+    
+   
+   
+
+
   }
 }
